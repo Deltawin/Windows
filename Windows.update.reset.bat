2d00e417855559c53cac5d1f714dce588de955b0
Deltawin <alexandre59135@outlook.fr>
2020-01-06 16:37:25 +0000

    Update Windows.update.reset.bat

--- Windows.update.reset.bat
+++ Windows.update.reset.bat
@@ -7,20 +7,14 @@ net stop msiserver
 net stop appidsvc
 
 ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
-pause
 
 ren C:\Windows\System32\catroot2 Catroot2.old
-pause
 
 del "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader Downloader.old
-pause
 
 Ren c:\windows\winsxs\pending.xml pending.old
-pause
-
 
 netsh winsock reset
-pause
 
 net start wuauserv
 net start cryptsvc
