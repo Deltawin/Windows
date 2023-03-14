ffb585667998496909f4d87c7665d93e2c33d197
Deltawin <alexandre59135@outlook.fr>
2023-03-14 19:00:45 +0100

    Delete Auto.bat

--- Auto.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-powercfg -h off
-
-PowerShell -Command "Get-AppXPackage | Remove-AppxPackage"
-
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control" /v SvcHostSplitThresholdInKB /t REG_DWORD /d 33554432 /f
-
-pause
